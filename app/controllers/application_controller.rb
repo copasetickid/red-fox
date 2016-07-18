533531f748eda82ea8fa37897d142aa559e599d0
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-17 22:12:20 -0400

    Adds an authentication layer to check for user's access token for API responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  attr_reader :current_user
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    authenticate_with_http_token do |token|
+      @current_user = User.find_by(access_token: token)
+    end
+
+    if @current_user.nil?
+      return render json: { error: "Unauthorized" }, status: 401
+    end
+  end
+
+  def not_authorized
+    render json: { error: "Unauthorized" }, status: 403
+  end
 end
