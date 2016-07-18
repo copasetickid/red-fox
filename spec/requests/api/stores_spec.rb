160934dd22947d73d3539a2987c8b0b076bee7fe
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-17 23:11:20 -0400

    Refactor requests specs to auth a user first

--- spec/requests/api/stores_spec.rb
+++ spec/requests/api/stores_spec.rb
@@ -1,32 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Store API", :type => :request do
+  let!(:owner) { create(:user) }
   let!(:store) { create(:store) }
   let(:json) { JSON.parse(response.body) }
 
 
-  it "displays all the stores" do
-    get stores_path
+  context "as an authenticated user" do
 
-    expect(response.content_type).to eq "application/vnd.api+json"
+    let(:headers) do
+      { "HTTP_AUTHORIZATION" => "Token token=#{owner.access_token}" }
+    end
 
-    expect(response.status).to eq 200
-    expect(json['data'].length).to eq 1
-  end
+    it "displays all the stores" do
+      get stores_path, headers: headers
 
-  it "returns valid store data" do
-    get store_path(store)
+      expect(response.content_type).to eq "application/vnd.api+json"
 
-    expect(response.status).to eq 200
+      expect(response.status).to eq 200
+      expect(json['data'].length).to eq 1
+    end
 
-    expect(json['data']['id']).to  eq store.id.to_s
-    expect(json['data']['attributes']['name']).to eq store.name
-  end
+    it "returns valid store data" do
+      get store_path(store), headers: headers
 
-  it "returns an JSON:API error when the requesting store data with invalid params" do
-    get store_path(id: "5")
+      expect(response.status).to eq 200
 
-    expect(response.status).to eq 404
-    expect(json['errors']).to eq "Wrong ID provided"
+      expect(json['data']['id']).to  eq store.id.to_s
+      expect(json['data']['attributes']['name']).to eq store.name
+    end
+
+    it "returns an JSON:API error when the requesting store data with invalid params" do
+      get store_path(id: "1000"), headers: headers
+
+      expect(response.status).to eq 404
+      expect(json['errors']).to eq "Wrong ID provided"
+    end
   end
 end
--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -2,39 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "User API", :type => :request do
   let!(:user) { create(:user) }
+  let(:json) { JSON.parse(response.body) }
 
+   context "as an authenticated user" do
 
-  it "displays all the users" do
-    get users_path
+    let(:headers) do
+      { "HTTP_AUTHORIZATION" => "Token token=#{user.access_token}" }
+    end
 
-    json = JSON.parse(response.body)
+    it "displays all the users" do
+      get users_path, headers: headers
 
-    expect(response.content_type).to eq "application/vnd.api+json"
+      expect(response.content_type).to eq "application/vnd.api+json"
 
-    expect(response.status).to eq 200
-    expect(json['data'].length).to eq 1
+      expect(response.status).to eq 200
+      expect(json['data'].length).to eq 1
+    end
 
+    it "returns valid user data" do
+      get user_path(user), headers: headers
 
-  end
+      expect(response.status).to eq 200
 
-  it "returns valid user data" do
-    get user_path(user)
+      expect(json['data']['id']).to  eq user.id.to_s
+      expect(json['data']['attributes']['email']).to eq user.email
+    end
 
-    json = JSON.parse(response.body)
+    it "returns an JSON:API error when the requesting user data with invalid" do
+      get user_path(id: "5"), headers: headers
 
-    expect(response.status).to eq 200
-
-    expect(json['data']['id']).to  eq user.id.to_s
-    expect(json['data']['attributes']['email']).to eq user.email
-  end
-
-  it "returns an JSON:API error when the requesting user data with invalid" do
-    get user_path(id: "5")
-
-    json = JSON.parse(response.body)
-
-    expect(response.status).to eq 404
-    expect(json['errors']).to eq "Wrong ID provided"
-
+      expect(response.status).to eq 404
+      expect(json['errors']).to eq "Wrong ID provided"
+    end
   end
 end
