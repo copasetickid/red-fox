0a1712e2db935d3d998237b5e3cfbcaa7e180d39
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-09-05 11:23:15 -0400

    Updates devise controller test module for specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,8 +58,8 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Warden::Test::Helpers, type: :feature
   config.after(type: :feature) { Warden.test_reset! }
-  config.include Devise::TestHelpers, :type => :controller
-  config.include Devise::TestHelpers, :type => :view
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers
 
 
@@ -84,4 +84,3 @@ Shoulda::Matchers.configure do |config|
      with.library :rails
   end
 end
-
